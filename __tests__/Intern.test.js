ecd8d02d6cfe67e31af9803b1f0ba3c620707884
MLanfear <maxjlanfear@gmail.com>
2022-11-13 02:48:44 -0800

    added engineer test, awaiting results

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -3,21 +3,21 @@ const Intern = require('../lib/Intern');
 
 // INTERN OBJECT CREATION  
 test('creates an Intern object', () => {
-    const intern = new Intern('Nicole', 90, 'nicole.elisaw@gmail', 'SFSU');
+    const intern = new Intern('MAX', 102, 'maxjlanfear@gmail.com', 'UOFO');
     
     expect(intern.school) .toEqual(expect.any(String));
 });
 
 // GETS SCHOOL
 test('gets employee school', () => {
-    const intern = new Intern('Nicole', 90, 'nicole.elisaw@gmail', 'SFSU');
+    const intern = new Intern('MAX', 102, 'maxjlanfear@gmail.com', 'UOFO');
     
     expect(intern.getSchool()).toEqual(expect.stringContaining(intern.school.toString()));
 });
 
 // ACQUIRE ROLE
 test('gets role of employee', () => {
-    const intern = new Intern('Nicole', 90, 'nicole.elisaw@gmail.com', 'SFSU');
+    const intern = new Intern('MAX', 102, 'maxjlanfear@gmail.com', 'UOFO');
 
     expect(intern.getRole()).toEqual("Intern");
 }); 
