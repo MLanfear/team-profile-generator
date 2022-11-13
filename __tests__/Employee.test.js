faae306b4200f42e351c28ccb724f51181baec0d
MLanfear <maxjlanfear@gmail.com>
2022-11-13 02:43:11 -0800

    testing ok

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -3,7 +3,7 @@ const Employee = require('../lib/Employee');
 
 // EMPLOYEE OBJECT CREATION 
 test('creates an employee object', () => {
-    const employee = new Employee('Nicole', 90, 'nicole.elisaw@gmail.com');
+    const employee = new Employee('MAX', 102, 'maxjlanfear@gmail.com');
 
     expect(employee.name).toEqual(expect.any(String));
     expect(employee.id).toEqual(expect.any(Number));
@@ -12,28 +12,28 @@ test('creates an employee object', () => {
 
 // TEST GETID() NAME 
 test('gets employee name', () => {
-    const employee = new Employee('Nicole', 90, 'nicole.elisaw@gmail.com');
+    const employee = new Employee('MAX', 102, 'maxjlanfear@gmail.com');
 
     expect(employee.getName()).toEqual(expect.any(String));
 });
 
 // TEST GETID() ID 
 test('gets employee ID', () => {
-    const employee = new Employee('Nicole', 90, 'nicole.elisaw@gmail.com');
+    const employee = new Employee('MAX', 102, 'maxjlanfear@gmail.com');
 
     expect(employee.getId()).toEqual(expect.any(Number));
 });
 
 // TEST GETEMAIL()
 test('gets employee email', () => {
-    const employee = new Employee('Nicole', 90, 'nicole.elisaw@gmail.com');
+    const employee = new Employee('MAX', 102, 'maxjlanfear@gmail.com');
 
     expect(employee.getEmail()).toEqual(expect.stringContaining(employee.email.toString()));
 });
 
 // TEST GETROLE()
 test('gets role of employee', () => {
-    const employee = new Employee('Nicole', 90, 'nicole.elisaw@gmail.com');
+    const employee = new Employee('MAX', 102, 'maxjlanfear@gmail.com');
 
     expect(employee.getRole()).toEqual("Employee");
 });
